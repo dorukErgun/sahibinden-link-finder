65fe03105cac44ee1f75a0bf2615699840b842e4
Doruk ERGÜN <32718897+dorukErgun@users.noreply.github.com>
2022-12-31 02:47:02 +0300

    Update GoogleSearchService.ts

--- services/GoogleSearchService.ts
+++ services/GoogleSearchService.ts
@@ -1,7 +1,7 @@
 const findSahibindenListingLink = async (
 	listingNo : string
 ) => {
-	const result = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${listingNo}&start=0&gl=tr`);
+	const result = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${listingNo}&start=0`);
 	const json = await result.json();
 	return json.items[0].link;
 }
