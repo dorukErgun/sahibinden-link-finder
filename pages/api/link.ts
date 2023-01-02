b51a3676ea305f7d9e15ac68e6ce375e4d2fb231
dorukErgun <doruk.ergun2017@gtu.edu.tr>
2023-01-02 18:21:38 +0300

    Update query

--- pages/api/link.ts
+++ pages/api/link.ts
@@ -5,7 +5,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req);
   const { listingNo } = req.query;
   if(!listingNo){
     res.status(400).json({
